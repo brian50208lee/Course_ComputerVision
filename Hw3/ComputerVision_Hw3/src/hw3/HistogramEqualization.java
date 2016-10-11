8212f5ad47b19c7d98faf8d45a99a6e5fed7c964
brianlee <brian50208lee@gmail.com>
2016-10-11 17:54:51 +0800

    add hw3

---
+++ Hw3/ComputerVision_Hw3/src/hw3/HistogramEqualization.java
@@ -0,0 +1,35 @@
+package hw3;
+
+import java.awt.image.BufferedImage;
+
+import com.sun.imageio.plugins.common.ImageUtil;
+
+import cv1.util.common.FileUtil;
+import cv1.util.cv.ImgUtil;
+
+public class HistogramEqualization {
+
+	public static final String inputFolder="input/";
+	public static final String inputFileName="lena.bmp";
+	public static final String outputFolder="output/";
+	public static void main(String[] args) {
+
+		//read image
+		System.out.println("reading img ...");
+		BufferedImage lena = FileUtil.readImg(inputFolder+inputFileName);
+		//ImgUtil.showImg(lena, "init");
+		
+		//histogram equalization
+		System.out.println("image histogram equalization ...");
+		BufferedImage equaImg = ImgUtil.histogramEqualization(lena);
+		//ImgUtil.showImg(equaImg, "equaImg");
+		
+		//output
+		FileUtil.writeImg(equaImg, outputFolder + "equaImg.png");
+		FileUtil.writeImg(ImgUtil.getHistogramImg(ImgUtil.getImgHistogramMatrix(ImgUtil.toGrayImage(lena))), outputFolder + "hist_before.png");
+		FileUtil.writeImg(ImgUtil.getHistogramImg(ImgUtil.getImgHistogramMatrix(equaImg)) ,outputFolder+ "hist_after.png");
+		
+		System.out.println("done");
+	}
+
+}
