5b1961bb2ce204bf1e2ccea1c75f78efd040a650
brianlee <brian50208lee@gmail.com>
2016-12-12 18:12:24 +0800

    print SNR

--- Hw/ComputerVision1/src/hw8/DemoNoiseRemoval.java
+++ Hw/ComputerVision1/src/hw8/DemoNoiseRemoval.java
@@ -25,21 +25,22 @@ public class DemoNoiseRemoval {
 		BufferedImage noise_gaussian_30 = NoiseGenerator.generateGaussianNoise(lena, 30);
 		BufferedImage noise_pepper_005 = NoiseGenerator.generateSaltAndPepperNoise(lena, 0.05);
 		BufferedImage noise_pepper_01 = NoiseGenerator.generateSaltAndPepperNoise(lena, 0.1);
+		System.out.printf("noise_gaussian_10 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, noise_gaussian_10));
+		System.out.printf("noise_gaussian_30 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, noise_gaussian_30));
+		System.out.printf("noise_pepper_005 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, noise_pepper_005));
+		System.out.printf("noise_pepper_01 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, noise_pepper_01));
 		
-		// show
-		//ImgUtil.showImg(lena, "lena");
-		//ImgUtil.showImg(noise_gaussian_10, "noise_gaussian_10");
-		//ImgUtil.showImg(noise_gaussian_30, "noise_gaussian_30");
-		//ImgUtil.showImg(noise_pepper_005, "noise_pepper_005");
-		//ImgUtil.showImg(noise_pepper_01, "noise_pepper_01");
-
 		// noise remove box filter 3x3
 		System.out.println("remove noise by 3x3 box filter ...");
 		BufferedImage rn_b3x3_g10 = NoiseRemover.boxFilter(noise_gaussian_10, NoiseRemover.FILTER_3x3);
 		BufferedImage rn_b3x3_g30 = NoiseRemover.boxFilter(noise_gaussian_30, NoiseRemover.FILTER_3x3);
 		BufferedImage rn_b3x3_p005 = NoiseRemover.boxFilter(noise_pepper_005, NoiseRemover.FILTER_3x3);
 		BufferedImage rn_b3x3_p01 = NoiseRemover.boxFilter(noise_pepper_01, NoiseRemover.FILTER_3x3);
-	
+		System.out.printf("rn_b3x3_g10 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_b3x3_g10));
+		System.out.printf("rn_b3x3_g30 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_b3x3_g30));
+		System.out.printf("rn_b3x3_p005 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_b3x3_p005));
+		System.out.printf("rn_b3x3_p01 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_b3x3_p01));
+
 
 		// noise remove box filter 5x5
 		System.out.println("remove noise by 5x5 box filter ...");
@@ -47,6 +48,11 @@ public class DemoNoiseRemoval {
 		BufferedImage rn_b5x5_g30 = NoiseRemover.boxFilter(noise_gaussian_30, NoiseRemover.FILTER_5x5);
 		BufferedImage rn_b5x5_p005 = NoiseRemover.boxFilter(noise_pepper_005, NoiseRemover.FILTER_5x5);
 		BufferedImage rn_b5x5_p01 = NoiseRemover.boxFilter(noise_pepper_01, NoiseRemover.FILTER_5x5);
+		System.out.printf("rn_b5x5_g10 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_b5x5_g10));
+		System.out.printf("rn_b5x5_g30 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_b5x5_g30));
+		System.out.printf("rn_b5x5_p005 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_b5x5_p005));
+		System.out.printf("rn_b5x5_p01 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_b5x5_p01));
+
 		
 
 		// noise remove median filter 3x3
@@ -55,7 +61,11 @@ public class DemoNoiseRemoval {
 		BufferedImage rn_m3x3_g30 = NoiseRemover.medianFilter(noise_gaussian_30, NoiseRemover.FILTER_3x3);
 		BufferedImage rn_m3x3_p005 = NoiseRemover.medianFilter(noise_pepper_005, NoiseRemover.FILTER_3x3);
 		BufferedImage rn_m3x3_p01 = NoiseRemover.medianFilter(noise_pepper_01, NoiseRemover.FILTER_3x3);
-		
+		System.out.printf("rn_m3x3_g10 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_m3x3_g10));
+		System.out.printf("rn_m3x3_g30 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_m3x3_g30));
+		System.out.printf("rn_m3x3_p005 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_m3x3_p005));
+		System.out.printf("rn_m3x3_p01 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_m3x3_p01));
+
 
 		// noise remove median filter 5x5
 		System.out.println("remove noise by 5x5 median filter ...");
@@ -63,6 +73,11 @@ public class DemoNoiseRemoval {
 		BufferedImage rn_m5x5_g30 = NoiseRemover.medianFilter(noise_gaussian_30, NoiseRemover.FILTER_5x5);
 		BufferedImage rn_m5x5_p005 = NoiseRemover.medianFilter(noise_pepper_005, NoiseRemover.FILTER_5x5);
 		BufferedImage rn_m5x5_p01 = NoiseRemover.medianFilter(noise_pepper_01, NoiseRemover.FILTER_5x5);
+		System.out.printf("rn_m5x5_g10 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_m5x5_g10));
+		System.out.printf("rn_m5x5_g30 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_m5x5_g30));
+		System.out.printf("rn_m5x5_p005 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_m5x5_p005));
+		System.out.printf("rn_m5x5_p01 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_m5x5_p01));
+
 
 		// opening the closing
 		System.out.println("remove noise by opening then closing ...");
@@ -74,6 +89,11 @@ public class DemoNoiseRemoval {
 		rn_opcls_p005 = GrayLevelMorphology.closing(rn_opcls_p005, "3-5-5-5-3", 0);
 		BufferedImage rn_opcls_p01 = GrayLevelMorphology.opening(noise_pepper_01, "3-5-5-5-3", 0);
 		rn_opcls_p01 = GrayLevelMorphology.closing(rn_opcls_p01, "3-5-5-5-3", 0);
+		System.out.printf("rn_opcls_g10 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_opcls_g10));
+		System.out.printf("rn_opcls_g30 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_opcls_g30));
+		System.out.printf("rn_opcls_p005 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_opcls_p005));
+		System.out.printf("rn_opcls_p01 SNR: %f\n",NoiseGenerator.signalToNoiseRatio(lena, rn_opcls_p01));
+
 		
 		
 		
