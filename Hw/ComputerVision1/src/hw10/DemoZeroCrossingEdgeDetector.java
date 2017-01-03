00afd4b81965c9ae11ec70979e2dc0e217087946
brianlee <brian50208lee@gmail.com>
2017-01-03 19:27:00 +0800

    add report

--- Hw/ComputerVision1/src/hw10/DemoZeroCrossingEdgeDetector.java
+++ Hw/ComputerVision1/src/hw10/DemoZeroCrossingEdgeDetector.java
@@ -8,42 +8,40 @@ import cv1.util.cv.edge.ZeroCrossingEdgeDetector;
 import cv1.util.cv.edge.ZeroCrossingEdgeDetector.MaskName;
 
 public class DemoZeroCrossingEdgeDetector {
-
+	
 	public static final String inputFolder="input/";
 	public static final String inputFileName="lena.bmp";
 	public static final String outputFolder="output/";
 	
 	public static void main(String[] args) {
-
-		
-		//read image
-		System.out.println("reading img ...");
+		// Reading image
+		System.out.println("Reading image ...");
 		BufferedImage lena = FileUtil.readImg(inputFolder+inputFileName);
 		lena = ImgUtil.toGrayImage(lena);
 		ImgUtil.showImg(lena, "lena");
 
 		// Laplace 
-		System.out.println("laplace ...");
+		System.out.println("Laplace Edge Detector ...");
 		BufferedImage laplace = ZeroCrossingEdgeDetector.operate(lena, 30, MaskName.Laplace);
 		ImgUtil.showImg(laplace, "laplace");
 
 		// MinVarLaplace 
-		System.out.println("minVarLaplace ...");
+		System.out.println("MinVarLaplace Edge Detector ...");
 		BufferedImage minVarLaplace = ZeroCrossingEdgeDetector.operate(lena, 15, MaskName.MinVarLaplace);
 		ImgUtil.showImg(minVarLaplace, "minVarLaplace");
 
 		// Laplace of Gaussian 
-		System.out.println("LoG ...");
+		System.out.println("Laplace of Gaussian Edge Detector ...");
 		BufferedImage LoG = ZeroCrossingEdgeDetector.operate(lena, 5000, MaskName.LoG);
 		ImgUtil.showImg(LoG, "LoG");
 		
-		// Differential of Gaussian 
-		System.out.println("DoG ...");
+		// Difference of Gaussian 
+		System.out.println("Difference of Gaussians Edge Detector ...");
 		BufferedImage DoG = ZeroCrossingEdgeDetector.operate(lena, 1, MaskName.DoG);
 		ImgUtil.showImg(DoG, "DoG");
 
-		// output image
-		System.out.println("output image ...");
+		// Output image
+		System.out.println("Output image ...");
 		FileUtil.writeImg(laplace, outputFolder + "laplace.png");
 		FileUtil.writeImg(minVarLaplace, outputFolder + "minVarLaplace.png");
 		FileUtil.writeImg(LoG, outputFolder + "LoG.png");
